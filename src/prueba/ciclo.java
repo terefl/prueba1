a8fad4b6623f28e57ce8536793a9355ac90b41e3
alumno <alumno@debian>
2023-04-13 08:56:43 +0200

    v3 desarrollo

--- src/prueba/ciclo.java
+++ src/prueba/ciclo.java
@@ -5,4 +5,30 @@ public class ciclo {
 	private String nombre;
 	private int duracion;
 	private String comentario;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	
 }
